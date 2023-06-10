4ca07bc79d92a9b1132ebaaab25f96fcf2e002c0
vladyslav-an <136080945+vladyslav-an@users.noreply.github.com>
2023-06-10 15:44:54 +0300

    Add files via upload

---
+++ HelloWorldController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleAppPostgre.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome()
+    {
+        return "This is the Welcome action method...";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleAppPostgre
+{
+    
+
+    class Program {
+        public static void Main(string[] args) {
+            var builder = WebApplication.CreateBuilder(args);
+    
+            builder.Services.AddControllers();
+            //builder.Services.AddSingleton<>();
+            builder.Services.AddEndpointsApiExplorer();
+
+            var app = builder.Build();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+
+   
+}
